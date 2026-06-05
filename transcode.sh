4b5de33d114d3b4344052e4a1706295f57f6c92f
hireki <hireki@proton.me>
2026-06-05 11:39:16 -0300

    [refactor]: change transcode.sh identation

--- transcode.sh
+++ transcode.sh
@@ -4,53 +4,55 @@ source ./utils.sh
 source ./flags.sh
 
 build_ordered_encoding_flags() {
-  local media="$1"
+    local media="$1"
 
-  initialize_state
+    initialize_state
 
-  local video_flags audio_flags subtitle_flags
-  read -ra video_flags < <(make_video_flags "$media")
-  read -ra audio_flags < <(make_audio_flags "$media")
-  read -ra subtitle_flags < <(make_subtitle_flags "$media")
+    local video_flags audio_flags subtitle_flags
+    read -ra video_flags < <(make_video_flags "$media")
+    read -ra audio_flags < <(make_audio_flags "$media")
+    read -ra subtitle_flags < <(make_subtitle_flags "$media")
 
-  has_pending_operations || { cleanup_state; return; }
+    has_pending_operations || {
+        cleanup_state
+        return
+    }
 
-  local flags=()
+    local flags=()
 
-  if is_burning_sub; then
-    read -ra video_flags < <(make_burning_sub_video_flags)
-    flags=( "${subtitle_flags[@]}" "${video_flags[@]}" "${audio_flags[@]}" )
-  else
-    flags=( "${video_flags[@]}" "${audio_flags[@]}" "${subtitle_flags[@]}" )
-  fi
+    if is_burning_sub; then
+        read -ra video_flags < <(make_burning_sub_video_flags)
+        flags=("${subtitle_flags[@]}" "${video_flags[@]}" "${audio_flags[@]}")
+    else
+        flags=("${video_flags[@]}" "${audio_flags[@]}" "${subtitle_flags[@]}")
+    fi
 
-  cleanup_state
+    cleanup_state
 
-  echo "${flags[@]}"
+    echo "${flags[@]}"
 }
 
 transcode() {
-  local media="$1"
-  local output="$2"
+    local media="$1"
+    local output="$2"
 
-  update_json ".duration" $(get_duration "$media") "$METADATA"
+    update_json ".duration" $(get_duration "$media") "$METADATA"
 
-  local decoding_flags encoding_flags
-  read -ra encoding_flags < <(build_ordered_encoding_flags "$media")
-  [[ "${#encoding_flags}" -eq 0 ]] && return 1
-  read -ra decoding_flags <<< "$VIDEO_DECODING_FLAGS"
+    local decoding_flags encoding_flags
+    read -ra encoding_flags < <(build_ordered_encoding_flags "$media")
+    [[ "${#encoding_flags}" -eq 0 ]] && return 1
+    read -ra decoding_flags <<<"$VIDEO_DECODING_FLAGS"
 
-  ffmpeg -v quiet -hide_banner -nostdin -progress pipe:1 \
-    "${decoding_flags[@]}" -i "$media" \
-    "${encoding_flags[@]}" "$output" |
-    parse_progress
+    ffmpeg -v quiet -hide_banner -nostdin -progress pipe:1 \
+        "${decoding_flags[@]}" -i "$media" \
+        "${encoding_flags[@]}" "$output" |
+        parse_progress
 
-  # The file's been processed. Increase counter.
-  local num_output_files=$(jq -r ".num_output_files" "$METADATA")
-  ((num_output_files++))
-  update_json ".num_output_files" "$num_output_files" "$METADATA"
+    # The file's been processed. Increase counter.
+    local num_output_files=$(jq -r ".num_output_files" "$METADATA")
+    ((num_output_files++))
+    update_json ".num_output_files" "$num_output_files" "$METADATA"
 }
-
 
 [[ "$1" =~ ^(-h|--help|-help)$ ]] && display_help
 
@@ -63,8 +65,8 @@ readarray -t files < <(find "$media_path" -maxdepth 1 -type f 2>/dev/null | sort
 update_json ".num_input_files" "${#files[@]}" "$METADATA"
 
 for media in "${files[@]}"; do
-  output=$(get_output_filename "$media" "$to_directory")
-  transcode "$media" "$output"
+    output=$(get_output_filename "$media" "$to_directory")
+    transcode "$media" "$output"
 done
 
 cleanup_metadata
